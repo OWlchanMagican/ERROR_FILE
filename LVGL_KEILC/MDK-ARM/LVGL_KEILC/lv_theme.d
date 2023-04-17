e9daeeba3a036b68a11176ad5fb5e9008ac8bf4f
OWlchanMagican <nguyentrietg@gmail.com>
2023-04-17 11:01:07 +0700

    file

---
+++ LVGL_KEILC/MDK-ARM/LVGL_KEILC/lv_theme.d
@@ -0,0 +1,192 @@
+lvgl_keilc/lv_theme.o: ..\GUI\lvgl\src\core\lv_theme.c \
+  ..\GUI\lvgl\src\core\..\..\lvgl.h ..\GUI\lvgl\src\misc\lv_log.h \
+  ..\GUI\lvgl\src\misc\..\lv_conf_internal.h \
+  C:\Keil_v5\ARM\ARMCLANG\Bin\..\include\stdint.h \
+  ..\GUI\lvgl\src\misc\..\lv_conf_kconfig.h ..\GUI\lv_conf.h \
+  ..\GUI\lvgl\src\misc\lv_types.h ..\GUI\lvgl\src\misc\lv_timer.h \
+  ..\GUI\lvgl\src\misc\..\hal\lv_hal_tick.h \
+  ..\GUI\lvgl\src\misc\..\hal\..\lv_conf_internal.h \
+  C:\Keil_v5\ARM\ARMCLANG\Bin\..\include\stdbool.h \
+  ..\GUI\lvgl\src\misc\lv_math.h ..\GUI\lvgl\src\misc\lv_mem.h \
+  C:\Keil_v5\ARM\ARMCLANG\Bin\..\include\stddef.h \
+  C:\Keil_v5\ARM\ARMCLANG\Bin\..\include\string.h \
+  ..\GUI\lvgl\src\misc\lv_async.h \
+  ..\GUI\lvgl\src\misc\lv_anim_timeline.h ..\GUI\lvgl\src\misc\lv_anim.h \
+  ..\GUI\lvgl\src\misc\lv_printf.h \
+  C:\Keil_v5\ARM\ARMCLANG\Bin\..\include\inttypes.h \
+  C:\Keil_v5\ARM\ARMCLANG\Bin\..\include\stdarg.h \
+  ..\GUI\lvgl\src\hal\lv_hal.h ..\GUI\lvgl\src\misc\..\hal\lv_hal_disp.h \
+  ..\GUI\lvgl\src\misc\..\hal\lv_hal.h \
+  ..\GUI\lvgl\src\misc\..\hal\..\draw\lv_draw.h \
+  ..\GUI\lvgl\src\misc\..\hal\..\draw\..\lv_conf_internal.h \
+  ..\GUI\lvgl\src\misc\..\hal\..\draw\..\misc\lv_style.h \
+  ..\GUI\lvgl\src\misc\..\font\lv_font.h \
+  ..\GUI\lvgl\src\misc\..\font\..\lv_conf_internal.h \
+  ..\GUI\lvgl\src\misc\..\font\lv_symbol_def.h \
+  ..\GUI\lvgl\src\misc\..\font\..\misc\lv_area.h \
+  ..\GUI\lvgl\src\misc\lv_color.h ..\GUI\lvgl\src\misc\lv_assert.h \
+  ..\GUI\lvgl\src\misc\lv_area.h ..\GUI\lvgl\src\misc\lv_txt.h \
+  ..\GUI\lvgl\src\misc\lv_bidi.h ..\GUI\lvgl\src\misc\lv_style_gen.h \
+  ..\GUI\lvgl\src\misc\..\hal\..\draw\..\misc\lv_txt.h \
+  ..\GUI\lvgl\src\misc\..\hal\..\draw\lv_img_decoder.h \
+  ..\GUI\lvgl\src\misc\..\hal\..\draw\lv_img_buf.h \
+  ..\GUI\lvgl\src\misc\..\hal\..\draw\..\misc\lv_color.h \
+  ..\GUI\lvgl\src\misc\..\hal\..\draw\..\misc\lv_area.h \
+  ..\GUI\lvgl\src\misc\..\hal\..\draw\..\misc\lv_fs.h \
+  ..\GUI\lvgl\src\misc\..\hal\..\draw\..\misc\lv_types.h \
+  ..\GUI\lvgl\src\misc\..\hal\..\draw\lv_img_cache.h \
+  ..\GUI\lvgl\src\misc\..\hal\..\draw\lv_draw_rect.h \
+  ..\GUI\lvgl\src\misc\..\hal\..\draw\sw\lv_draw_sw_gradient.h \
+  ..\GUI\lvgl\src\misc\..\hal\..\draw\sw\..\..\misc\lv_color.h \
+  ..\GUI\lvgl\src\misc\..\hal\..\draw\sw\..\..\misc\lv_style.h \
+  ..\GUI\lvgl\src\misc\..\hal\..\draw\sw\lv_draw_sw_dither.h \
+  ..\GUI\lvgl\src\misc\..\hal\..\draw\sw\..\..\core\lv_obj_pos.h \
+  ..\GUI\lvgl\src\core\..\misc\lv_area.h \
+  ..\GUI\lvgl\src\misc\..\hal\..\draw\lv_draw_label.h \
+  ..\GUI\lvgl\src\misc\..\hal\..\draw\..\misc\lv_bidi.h \
+  ..\GUI\lvgl\src\misc\..\hal\..\draw\lv_draw_img.h \
+  ..\GUI\lvgl\src\misc\..\hal\..\draw\lv_draw_line.h \
+  ..\GUI\lvgl\src\misc\..\hal\..\draw\lv_draw_triangle.h \
+  ..\GUI\lvgl\src\misc\..\hal\..\draw\lv_draw_arc.h \
+  ..\GUI\lvgl\src\misc\..\hal\..\draw\lv_draw_mask.h \
+  ..\GUI\lvgl\src\misc\..\hal\..\draw\..\misc\lv_math.h \
+  ..\GUI\lvgl\src\misc\..\hal\..\draw\lv_draw_transform.h \
+  ..\GUI\lvgl\src\misc\..\hal\..\draw\lv_draw_layer.h \
+  ..\GUI\lvgl\src\misc\..\hal\..\misc\lv_color.h \
+  ..\GUI\lvgl\src\misc\..\hal\..\misc\lv_area.h \
+  ..\GUI\lvgl\src\misc\..\hal\..\misc\lv_ll.h \
+  ..\GUI\lvgl\src\misc\..\hal\..\misc\lv_timer.h \
+  ..\GUI\lvgl\src\misc\..\hal\lv_hal_indev.h \
+  ..\GUI\lvgl\src\core\lv_obj.h \
+  ..\GUI\lvgl\src\core\..\lv_conf_internal.h \
+  ..\GUI\lvgl\src\core\..\misc\lv_style.h \
+  ..\GUI\lvgl\src\core\..\misc\lv_types.h \
+  ..\GUI\lvgl\src\core\..\misc\lv_color.h \
+  ..\GUI\lvgl\src\core\..\misc\lv_assert.h \
+  ..\GUI\lvgl\src\core\..\hal\lv_hal.h \
+  ..\GUI\lvgl\src\core\lv_obj_tree.h ..\GUI\lvgl\src\core\lv_obj_pos.h \
+  ..\GUI\lvgl\src\core\lv_obj_scroll.h \
+  ..\GUI\lvgl\src\core\..\misc\lv_anim.h \
+  ..\GUI\lvgl\src\core\lv_obj_style.h \
+  ..\GUI\lvgl\src\core\..\misc\lv_bidi.h \
+  ..\GUI\lvgl\src\core\lv_obj_style_gen.h \
+  ..\GUI\lvgl\src\core\lv_obj_draw.h \
+  ..\GUI\lvgl\src\core\..\draw\lv_draw.h \
+  ..\GUI\lvgl\src\core\lv_obj_class.h ..\GUI\lvgl\src\core\lv_event.h \
+  ..\GUI\lvgl\src\core\lv_group.h ..\GUI\lvgl\src\core\..\misc\lv_ll.h \
+  ..\GUI\lvgl\src\core\lv_indev.h \
+  ..\GUI\lvgl\src\core\..\hal\lv_hal_indev.h \
+  ..\GUI\lvgl\src\core\lv_refr.h ..\GUI\lvgl\src\core\lv_disp.h \
+  ..\GUI\lvgl\src\core\lv_theme.h ..\GUI\lvgl\src\core\..\core\lv_obj.h \
+  ..\GUI\lvgl\src\font\lv_font.h ..\GUI\lvgl\src\font\lv_font_loader.h \
+  ..\GUI\lvgl\src\font\lv_font_fmt_txt.h \
+  ..\GUI\lvgl\src\widgets\lv_arc.h \
+  ..\GUI\lvgl\src\widgets\..\lv_conf_internal.h \
+  ..\GUI\lvgl\src\widgets\..\core\lv_obj.h \
+  ..\GUI\lvgl\src\widgets\lv_btn.h ..\GUI\lvgl\src\widgets\lv_img.h \
+  ..\GUI\lvgl\src\widgets\..\misc\lv_fs.h \
+  ..\GUI\lvgl\src\widgets\..\draw\lv_draw.h \
+  ..\GUI\lvgl\src\widgets\lv_label.h \
+  ..\GUI\lvgl\src\widgets\..\font\lv_font.h \
+  ..\GUI\lvgl\src\widgets\..\font\lv_symbol_def.h \
+  ..\GUI\lvgl\src\widgets\..\misc\lv_txt.h \
+  ..\GUI\lvgl\src\widgets\lv_line.h ..\GUI\lvgl\src\widgets\lv_table.h \
+  ..\GUI\lvgl\src\widgets\lv_checkbox.h ..\GUI\lvgl\src\widgets\lv_bar.h \
+  ..\GUI\lvgl\src\widgets\..\misc\lv_anim.h \
+  ..\GUI\lvgl\src\widgets\lv_slider.h \
+  ..\GUI\lvgl\src\widgets\lv_btnmatrix.h \
+  ..\GUI\lvgl\src\widgets\lv_dropdown.h \
+  ..\GUI\lvgl\src\widgets\..\widgets\lv_label.h \
+  ..\GUI\lvgl\src\widgets\lv_roller.h \
+  ..\GUI\lvgl\src\widgets\lv_textarea.h \
+  ..\GUI\lvgl\src\widgets\lv_canvas.h \
+  ..\GUI\lvgl\src\widgets\..\widgets\lv_img.h \
+  ..\GUI\lvgl\src\widgets\..\draw\lv_draw_img.h \
+  ..\GUI\lvgl\src\widgets\lv_switch.h ..\GUI\lvgl\src\draw\lv_draw.h \
+  ..\GUI\lvgl\src\lv_api_map.h ..\GUI\lvgl\src\misc\..\..\lvgl.h \
+  ..\GUI\lvgl\src\extra\lv_extra.h \
+  ..\GUI\lvgl\src\extra\layouts\lv_layouts.h \
+  ..\GUI\lvgl\src\extra\layouts\flex\lv_flex.h \
+  ..\GUI\lvgl\src\extra\layouts\flex\..\..\..\core\lv_obj.h \
+  ..\GUI\lvgl\src\extra\layouts\grid\lv_grid.h \
+  ..\GUI\lvgl\src\extra\layouts\grid\..\..\..\core\lv_obj.h \
+  ..\GUI\lvgl\src\extra\libs\lv_libs.h \
+  ..\GUI\lvgl\src\extra\libs\bmp\lv_bmp.h \
+  ..\GUI\lvgl\src\extra\libs\bmp\..\..\..\lv_conf_internal.h \
+  ..\GUI\lvgl\src\extra\libs\fsdrv\lv_fsdrv.h \
+  ..\GUI\lvgl\src\extra\libs\fsdrv\..\..\..\lv_conf_internal.h \
+  ..\GUI\lvgl\src\extra\libs\png\lv_png.h \
+  ..\GUI\lvgl\src\extra\libs\png\..\..\..\lv_conf_internal.h \
+  ..\GUI\lvgl\src\extra\libs\gif\lv_gif.h \
+  ..\GUI\lvgl\src\extra\libs\gif\..\..\..\lvgl.h \
+  ..\GUI\lvgl\src\extra\libs\qrcode\lv_qrcode.h \
+  ..\GUI\lvgl\src\extra\libs\qrcode\..\..\..\lvgl.h \
+  ..\GUI\lvgl\src\extra\libs\sjpg\lv_sjpg.h \
+  ..\GUI\lvgl\src\extra\libs\freetype\lv_freetype.h \
+  ..\GUI\lvgl\src\extra\libs\freetype\..\..\..\lvgl.h \
+  ..\GUI\lvgl\src\extra\libs\rlottie\lv_rlottie.h \
+  ..\GUI\lvgl\src\extra\libs\rlottie\..\..\..\lvgl.h \
+  ..\GUI\lvgl\src\extra\libs\ffmpeg\lv_ffmpeg.h \
+  ..\GUI\lvgl\src\extra\libs\ffmpeg\..\..\..\lvgl.h \
+  ..\GUI\lvgl\src\extra\others\lv_others.h \
+  ..\GUI\lvgl\src\extra\others\snapshot\lv_snapshot.h \
+  ..\GUI\lvgl\src\extra\others\snapshot\..\..\..\lv_conf_internal.h \
+  ..\GUI\lvgl\src\extra\others\snapshot\..\..\..\core\lv_obj.h \
+  ..\GUI\lvgl\src\extra\others\monkey\lv_monkey.h \
+  ..\GUI\lvgl\src\extra\others\monkey\..\..\..\lvgl.h \
+  ..\GUI\lvgl\src\extra\others\gridnav\lv_gridnav.h \
+  ..\GUI\lvgl\src\extra\others\gridnav\..\..\..\core\lv_obj.h \
+  ..\GUI\lvgl\src\extra\others\fragment\lv_fragment.h \
+  ..\GUI\lvgl\src\extra\others\fragment\..\..\..\lv_conf_internal.h \
+  ..\GUI\lvgl\src\extra\others\imgfont\lv_imgfont.h \
+  ..\GUI\lvgl\src\extra\others\imgfont\..\..\..\lvgl.h \
+  ..\GUI\lvgl\src\extra\others\msg\lv_msg.h \
+  ..\GUI\lvgl\src\extra\others\msg\..\..\..\core\lv_obj.h \
+  ..\GUI\lvgl\src\extra\others\ime\lv_ime_pinyin.h \
+  ..\GUI\lvgl\src\extra\others\ime\..\..\..\lvgl.h \
+  ..\GUI\lvgl\src\extra\themes\lv_themes.h \
+  ..\GUI\lvgl\src\extra\themes\default\lv_theme_default.h \
+  ..\GUI\lvgl\src\extra\themes\default\..\..\..\core\lv_obj.h \
+  ..\GUI\lvgl\src\extra\themes\mono\lv_theme_mono.h \
+  ..\GUI\lvgl\src\extra\themes\mono\..\..\..\core\lv_obj.h \
+  ..\GUI\lvgl\src\extra\themes\basic\lv_theme_basic.h \
+  ..\GUI\lvgl\src\extra\themes\basic\..\..\..\core\lv_obj.h \
+  ..\GUI\lvgl\src\extra\widgets\lv_widgets.h \
+  ..\GUI\lvgl\src\extra\widgets\animimg\lv_animimg.h \
+  ..\GUI\lvgl\src\extra\widgets\animimg\..\..\..\lvgl.h \
+  ..\GUI\lvgl\src\extra\widgets\calendar\lv_calendar.h \
+  ..\GUI\lvgl\src\extra\widgets\calendar\..\..\..\widgets\lv_btnmatrix.h \
+  ..\GUI\lvgl\src\extra\widgets\calendar\lv_calendar_header_arrow.h \
+  ..\GUI\lvgl\src\extra\widgets\calendar\..\..\..\core\lv_obj.h \
+  ..\GUI\lvgl\src\extra\widgets\calendar\lv_calendar_header_dropdown.h \
+  ..\GUI\lvgl\src\extra\widgets\chart\lv_chart.h \
+  ..\GUI\lvgl\src\extra\widgets\chart\..\..\..\lvgl.h \
+  ..\GUI\lvgl\src\extra\widgets\keyboard\lv_keyboard.h \
+  ..\GUI\lvgl\src\extra\widgets\keyboard\..\..\..\widgets\lv_btnmatrix.h \
+  ..\GUI\lvgl\src\extra\widgets\list\lv_list.h \
+  ..\GUI\lvgl\src\extra\widgets\list\..\..\..\core\lv_obj.h \
+  ..\GUI\lvgl\src\extra\widgets\list\..\..\layouts\flex\lv_flex.h \
+  ..\GUI\lvgl\src\extra\widgets\menu\lv_menu.h \
+  ..\GUI\lvgl\src\extra\widgets\menu\..\..\..\core\lv_obj.h \
+  ..\GUI\lvgl\src\extra\widgets\msgbox\lv_msgbox.h \
+  ..\GUI\lvgl\src\extra\widgets\msgbox\..\..\..\lvgl.h \
+  ..\GUI\lvgl\src\extra\widgets\meter\lv_meter.h \
+  ..\GUI\lvgl\src\extra\widgets\meter\..\..\..\lvgl.h \
+  ..\GUI\lvgl\src\extra\widgets\spinbox\lv_spinbox.h \
+  ..\GUI\lvgl\src\extra\widgets\spinbox\..\..\..\lvgl.h \
+  ..\GUI\lvgl\src\extra\widgets\spinner\lv_spinner.h \
+  ..\GUI\lvgl\src\extra\widgets\spinner\..\..\..\lvgl.h \
+  ..\GUI\lvgl\src\extra\widgets\tabview\lv_tabview.h \
+  ..\GUI\lvgl\src\extra\widgets\tabview\..\..\..\lvgl.h \
+  ..\GUI\lvgl\src\extra\widgets\tileview\lv_tileview.h \
+  ..\GUI\lvgl\src\extra\widgets\tileview\..\..\..\core\lv_obj.h \
+  ..\GUI\lvgl\src\extra\widgets\win\lv_win.h \
+  ..\GUI\lvgl\src\extra\widgets\win\..\..\..\lvgl.h \
+  ..\GUI\lvgl\src\extra\widgets\colorwheel\lv_colorwheel.h \
+  ..\GUI\lvgl\src\extra\widgets\colorwheel\..\..\..\lvgl.h \
+  ..\GUI\lvgl\src\extra\widgets\led\lv_led.h \
+  ..\GUI\lvgl\src\extra\widgets\led\..\..\..\lvgl.h \
+  ..\GUI\lvgl\src\extra\widgets\imgbtn\lv_imgbtn.h \
+  ..\GUI\lvgl\src\extra\widgets\imgbtn\..\..\..\lvgl.h \
+  ..\GUI\lvgl\src\extra\widgets\span\lv_span.h \
+  ..\GUI\lvgl\src\extra\widgets\span\..\..\..\lvgl.h
